3544324bfea371240ccf1bba0afb05ee39bef5b9
Cristian Franco <cfrancobedoya@gmail.com>
2020-09-07 17:19:34 -0500

    Updating our posts

--- blog/app/Http/Controllers/Backend/PostController.php
+++ blog/app/Http/Controllers/Backend/PostController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\Post;
-// use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
+
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
@@ -83,9 +84,17 @@ class PostController extends Controller
      * @param  \App\Post  $post
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post)
+    public function update(PostRequest $request, Post $post)
     {
-        //
+        $post->update($request->all());
+
+        if ($request->file('file')) {
+            Storage::disk('public')->delete($post->image);
+            $post->image = $request->file('file')->store('posts', 'public');
+            $post->save();
+        }
+
+        return back()->with('status', 'Successfully updated');
     }
 
     /**
@@ -96,6 +105,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Storage::disk('public')->delete($post->image);
+        $post->delete();
+
+        return back()->with('status', 'Successfully removed');
     }
 }
